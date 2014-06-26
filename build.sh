ebbcf3ba7f1a36f9a8d1f1bbacbf57f5d2b725fa
Ronie Salgado <roniesalg@gmail.com>
2014-06-26 03:00:13 -0400

    Fixed a script.

--- build.sh
+++ build.sh
@@ -1,6 +1,16 @@
 #!/bin/sh
 
+TOP=`pwd`
+
+# Build Bullet.
+cd $TOP
 sh ./buildBullet.sh
-sh ./bindings/make-bindings.sh
+
+# Make the bindings
+cd $TOP/bindings
+sh ./make-bindings.sh
+
+# Copy to the distribution folder.
+cd $TOP
 sh ./copyToDist.sh
 
